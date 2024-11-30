5e75742e570e87f59f7f7dd1b9d1b9ab1666639a
jerax <15604215906@163.com>
2024-11-30 23:40:39 +0800

    chore: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
+import  type{BlockState} from '~/types'
 
 
-interface BlockState {
-  x: number,
-  y: number,
-  revealed?: boolean,
-  mine?: boolean,
-  flagged?: boolean,
-  adjacentMines: number
-}
+
 const WIDTH = 10
 const HEIGHT = 10
-const state = reactive(
+const state = ref(
   Array.from({ length: HEIGHT }, (_, y) =>
     Array.from({ length: WIDTH },
       (_, x): BlockState => ({
@@ -22,7 +16,7 @@ const state = reactive(
 )
 
 function genarateMines(initial: BlockState) {
-  for (const row of state) {
+  for (const row of state.value) {
     for (const block of row) {
       if (Math.abs(initial.x - block.x) < 1) {
         continue
@@ -61,7 +55,7 @@ const numberColors = [
 ]
 
 function updateNumbers() {
-  state.forEach((row) => {
+  state.value.forEach((row) => {
     row.forEach((block) => {
       if (block.mine)
         return
@@ -81,7 +75,7 @@ function getSibLings(block: BlockState) {
     if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
       return undefined;
 
-    return state[y2][x2]
+    return state.value[y2][x2]
   })
     .filter(Boolean) as BlockState[]
 
@@ -92,17 +86,25 @@ function expendZero(block: BlockState) {
     return
 
   getSibLings(block).forEach((s) => {
-    if (!s.revealed){
-    s.revealed = true
-    expendZero(s)
+    if (!s.revealed) {
+      s.revealed = true
+      expendZero(s)
     }
   })
 
 }
 
 let mineGenarated = false
-const dev = true
-function onClick(block: BlockState) {
+const dev = false
+
+function onRightClick(block: BlockState) {
+  if (block.revealed)
+    return
+  block.flagged = !block.flagged
+
+}
+function onClick(e: MouseEvent, block: BlockState) {
+  console.log(e)
   if (!mineGenarated) {
     genarateMines(block)
     mineGenarated = true
@@ -112,17 +114,36 @@ function onClick(block: BlockState) {
     alert('Game Over')
   }
   expendZero(block)
+
 }
 
 function getBlockClass(block: BlockState) {
-  if (!block.revealed) {
+  if (block.flagged) {
     return 'bg-gray-500/10'
+  }
+  if (!block.revealed) {
+    return 'bg-gray-500/10 hover:bg-gray-500/20'
   }
   return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
 }
 
+watchEffect(() => {
+  checkGameState()
+})
 
-updateNumbers()
+function checkGameState() {
+  if (!mineGenarated) {
+    return
+  }
+  const blocks = state.value.flat()// flat() 将嵌套的数组展平
+  if (blocks.every((block) => block.revealed || block.flagged)) {
+    if (blocks.some((block) => block.flagged && block.mine)) {
+      alert('U Cheat!')
+    } else {
+      alert('You Win')
+    }
+  }
+}
 </script>
 
 <template>
@@ -131,8 +152,12 @@ updateNumbers()
     <div p5>
       <div v-for="row, y in state" :key="y" flex="~" items-center justify-center>
         <button v-for="(block, x) in row" :key="x" flex="~" items-center justify-center h-10 w-10 m="0.5"
-          hover="bg-gray/10" border=" 1 gray-400/10 " :class="getBlockClass(block)" @click="onClick(block)">
-          <template v-if="block.revealed || dev">
+          hover="bg-gray/10" border=" 1 gray-400/10 " :class="getBlockClass(block)" @click="onClick($event,
+            block)" @contextmenu.prevent="onRightClick(block)">
+          <template v-if="block.flagged">
+            <div i-mdi-flag text-red></div>
+          </template>
+          <template v-else-if="block.revealed || dev">
             <div v-if="block.mine" i-mdi-mine>x</div>
             <div v-else>{{ block.adjacentMines }}</div>
           </template>
